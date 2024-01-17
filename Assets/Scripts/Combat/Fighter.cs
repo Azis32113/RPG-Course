cdff3409c36d99f076a21157cfffe1b13876d630
Amirrulloh Azis <amirulloh.azis@gmail.com>
2024-01-17 11:19:55 +0700

    116. Shoot Projectiles

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -78,7 +78,20 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) return;
-            target.TakeDamage(currentWeapon.GetDamage());
+
+            if (currentWeapon.HasProjectile())
+            {
+                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target);
+            }
+
+            else target.TakeDamage(currentWeapon.GetDamage());
+
+        }
+
+        // Animation Event
+        void Shoot()
+        {
+            Hit();
         }
 
         private bool GetIsInRange()
