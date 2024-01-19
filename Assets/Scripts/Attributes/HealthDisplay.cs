e6bb338362b296b4f0c9c42f76f74a6bfe953110
Amirrulloh Azis <amirulloh.azis@gmail.com>
2024-01-19 11:12:52 +0700

    132. Debug Health Display

---
+++ Assets/Scripts/Attributes/HealthDisplay.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+namespace RPG.Attributes
+{    
+    class HealthDisplay : MonoBehaviour 
+    {
+        Health health;
+
+        void Awake() {
+            health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        }
+
+        void Update() {
+            GetComponent<TextMeshProUGUI>().text = string.Format("{0:0}%", health.GetPercentage());
+        }
+    }
+}
