5d52354891af12292c069f7b5a2af6a92cabb236
Amirrulloh Azis <amirulloh.azis@gmail.com>
2024-01-24 11:23:14 +0700

    165. Using Unity Events

--- Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -7,12 +7,7 @@ namespace RPG.UI.DamageText
     public class DamageTextSpawner : MonoBehaviour
     {
         [SerializeField] DamageText damageTextPrefab = null;
-
-        void Start()
-        {
-            Spawn(27);
-        }
-
+        
         public void Spawn(float damageAmount)
         {
             DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
